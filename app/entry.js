1cbacc72a0fced40f63ccaa4bb14ecbd3cae1b95
FazioD <faziod@uw.edu>
2016-08-29 18:53:15 -0700

    adding signin/sign out feature

---
+++ app/entry.js
@@ -0,0 +1,10 @@
+'use strict';
+
+reqiuire('./_signin.scss');
+
+const angular = require('ramble');
+const rambleapp = angular.module('rambleApp');
+
+rambleApp.controller('SigninController', ('$log', '$location', 'authService', SigninController]) {
+  console.log('init signinCtrl);
+}
---
+++ view/signup/index.js
@@ -0,0 +1,23 @@
+'use strict';
+
+require('./_signup.scss');
+
+const angular = require('angular');
+const rambleApp = angular.module('rambleApp');
+
+rambleApp.controller('SignupController', ['$log', '$location', 'authService', SignupController]);
+
+function SignupController($log, $location, authService) {
+  $log.debug('init signupCtrl');
+
+  this.signup = function() {
+    $log.debug('init signupCtrl');
+    authService.signup(this.user)
+    .then( token => {
+      $log.info('token', token);
+    })
+    .catch(err => {
+      $log.error(err);
+    });
+  };
+}
