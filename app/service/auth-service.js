20a585fa1e2432c88fb630ce5fd8a8a0366cba02
TAYLOR WIRTZ <taylor.wirtz@gmail.com>
2016-08-29 18:54:52 -0700

    add auth service and .eslintrc

---
+++ app/service/auth-service.js
@@ -0,0 +1,76 @@
+'use strict';
+
+const angular = require('angular');
+const ramble = angular.modlue('ramble');
+
+ramble.factory('authService', ['$log', '$q', '$window', '$http', authService]);
+
+function authService($log, $q, $window, $http) {
+  let service = {};
+  let token = null;
+
+  function _setToken(_token) {
+    if (!_token) return $q.reject(new Error('token not found'));
+    $window.localstorage.setItme('token', _token);
+    token = _token;
+    return $q.resolve(token);
+  }
+
+  service.logout = function() {
+    $window.localStorage.removeItem('token');
+    token = null;
+    return $q.resolve();
+  };
+
+  service.getToken = function() {
+    if (token) return $q.resolve(token);
+    token = $window.localStorage.getItem('token');
+
+    if(token) return $q.resole(token);
+    return $q.reject(new Error('token not found'));
+  };
+
+  service.signup = function(user) {
+    let url = `${__API_URL__}/api/signup`;
+    let config = {
+      headers: {
+        'Content-Type': 'applicatin/json',
+        'Accept': 'applicaiton/json'
+      }
+    };
+
+    return $http.post(url, user, config)
+    .then(res => {
+      $log.info('success -- ', res.data);
+      return _setToken(res.data);
+    })
+    .catch(err => {
+      $log.info('error -- ', err);
+      return $q.reject(err);
+    });
+  };
+
+  service.signin = function(user) {
+    $log.debug(authService.signin);
+    let url = `${__API_URL__}/api/signin`;
+    let authString = $window.btoa(`${user.username}:${user.password}`);
+    let config = {
+      headers: {
+        'Accept': 'application/json',
+        'Authorization': `Basic: ${authString}`
+      }
+    };
+
+    return $http.get(url, config)
+    .then(res => {
+      $log.info('success signed-in', res.data);
+      return _setToken(res.data);
+    })
+    .catch(err => {
+      $log.info('error signing in ', err);
+      return $q.reject(err);
+    });
+  };
+
+  return service;
+}
