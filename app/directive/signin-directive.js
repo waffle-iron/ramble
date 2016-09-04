cc23980f28d80a51d73353ce4a0efbc4bb7822fd
FazioD <faziod@uw.edu>
2016-09-04 11:05:45 -0700

    create signin directive to signinForm validation

---
+++ app/directive/signin-directive.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const angular = require('angular');
+
+angular.module('ramble')
+.component('signinForm',  {
+  templateUrl: 'signin.html',
+  controller: function SigninController() {}
+});
