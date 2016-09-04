dce7b7aab39392afa1530eca9650568adf12197c
TAYLOR WIRTZ <taylor.wirtz@gmail.com>
2016-09-04 11:44:25 -0700

    entry list shows title, border, and body preview

--- app/component/entry-list-item/index.js
+++ app/component/entry-list-item/index.js
@@ -7,7 +7,20 @@ const ramble = angular.module('ramble');
 
 ramble.component('rambleEntryListItem', {
   bindings: {
-    'entry':'='
+    entry:'='
   },
-  template: require('./entry-list-item.html')
+  template: require('./entry-list-item.html'),
+  controller: 'EntryListItemController'
 });
+
+ramble.controller('EntryListItemController', [EntryListItemController]);
+
+function EntryListItemController() {
+  this.bodyPreview = null;
+
+  if (this.entry.body.length > 75) {
+    this.bodyPreview = this.entry.body.slice(0, 74) + '...';
+  } else {
+    this.bodyPreview = this.entry.body;
+  }
+}
